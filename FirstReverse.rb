c13d0a84750f4138b96b8603be8ad25be12e524d
HanbyeolJung <hanbber@hanmail.net>
2015-07-27 23:55:24 +0900

    Update FirstReverse.rb

--- FirstReverse.rb
+++ FirstReverse.rb
@@ -1,1 +1,29 @@
-dd
+def FirstReverse(str)
+
+  # code goes here
+  return str.reverse
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstReverse(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
