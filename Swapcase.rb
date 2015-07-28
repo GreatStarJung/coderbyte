d173851a25a3370fd960b01937f6d4695d378726
HanbyeolJung <hanbber@hanmail.net>
2015-07-28 15:52:31 +0900

    Create Swapcase.rb

---
+++ Swapcase.rb
@@ -0,0 +1,10 @@
+def SwapCase(str)
+
+  dd = str.swapcase
+  return dd
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SwapCase(STDIN.gets)           
